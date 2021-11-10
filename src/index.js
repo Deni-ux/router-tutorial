c2fd0c4f5024808c26c691c6f9e27554da305c0c
Denisa Haka <denisa.haka@ymail.com>
2021-11-10 16:33:18 +0000

    learned about index routes, search params

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import Expenses from './routes/Expenses';
 import Invoices from './routes/Invoices';
-
+import Invoice from './routes/Invoice';
 
 const rootElement = document.getElementById("root");
 
@@ -16,6 +16,21 @@ render(
     <Route path="/" element={<App />} />
     <Route path="expenses" element={<Expenses />} />
     <Route path="invoices" element={<Invoices />} />
+    <Route index
+     element={
+       <main style={{padding:"1rem"}}>
+         <p>Select an invoice</p>
+       </main>
+         }
+         />
+    <Route path=":invoiceId" element={<Invoice />}/>
+    
+    <Route path="*" element={
+      <main style={{ padding: "1rem"}}>
+        <p>Oops, there's nothing here!</p>
+      </main>  
+    }
+    />
     
     </Routes>
   </BrowserRouter>,
---
+++ src/routes/Invoice.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { getInvoice } from "../Data";
+
+export default function Invoice(){
+let params = useParams();
+let invoice = getInvoice(parseInt(params.invoiceId, 10));
+
+    return (
+        <div className="Invoice" style={{padding:"1rem"}}>
+            <h2>Total due: {invoice.amount}</h2>
+            <p>
+                {invoice.name}: {invoice.number}
+            </p>
+            <p>Due date: {invoice.due}</p>
+        </div>
+    )
+}
